17f8557563ad2281cd41afef419106270a164828
momaotou <21xxxxxx@buaa.edu.cn>
2024-05-27 23:51:02 +0800

    主页背景

--- Frontend/Epp_FrontEnd/src/components/Search/SearchPage.vue
+++ Frontend/Epp_FrontEnd/src/components/Search/SearchPage.vue
@@ -1,15 +1,10 @@
 <template>
-  <div>
-    <el-row>
-      <el-col :span="24">
-        <el-header style="font-size: 40px; line-height: 60px; margin-top: 100px">文献调研</el-header>
+  <div class="searchPage">
+    <el-row style="margin-top: 30px;">
+      <el-col :span="20" :offset="2">
+        <search-input />
       </el-col>
     </el-row>
-      <el-row style="margin-top: 30px;">
-        <el-col :span="20" :offset="2">
-          <search-input />
-        </el-col>
-      </el-row>
   </div>
 </template>
 
@@ -23,5 +18,31 @@ export default {
 </script>
 
 <style scoped>
+.searchPage {
+  position: relative;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  padding: 20px;
+  padding-top: 50px;
+  min-height: 100vh;
+  background-image: url('../../assets/dashboardBack2.png');
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center bottom;
+  background-attachment: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+}
 
+.el-row {
+  width: 100%;
+}
+
+.el-col {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
